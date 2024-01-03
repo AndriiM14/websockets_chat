c12b1db4c2eccaec7ff5ab434bb6ff522eb25ac1
Andrii M <andrii.marusyk.knm.2020@lpnu.ua>
2024-01-03 03:01:55 +0200

    implemented simple chat server

--- server/lib/server.dart
+++ server/lib/server.dart
@@ -1,41 +1,52 @@
-import "dart:io" show HttpServer, HttpRequest, stderr;
-import "dart:convert" show utf8;
+import "dart:io"
+    show HttpServer, HttpRequest, WebSocketTransformer, WebSocket, stderr;
 
 class Server {
   final String ip;
   final int port;
 
+  List<WebSocket> connections = [];
+
   Server(this.ip, this.port);
 
-  void log(HttpRequest request) {
-    print(
-        "[+]${request.method}: ${request.headers.toString().split("\n").join(" ")}");
+  void logRequest(HttpRequest request) {
+    print("[+]${request.connectionInfo?.remoteAddress} connected.");
+  }
+
+  void logError(Object error) {
+    stderr.write("[!]Error: ${error.toString()}.\n");
   }
 
   void listen() async {
     try {
       final httpServer = await HttpServer.bind(ip, port);
-      print("[+]Server listening on $ip:$port");
+      print("[+]Server listening on $ip:$port.");
 
       try {
         await httpServer.forEach((final request) async {
-          log(request);
-          switch (request.method) {
-            case "GET":
-              request.response.write("Hello from dart server\n");
-              break;
-            case "POST":
-              final body = await utf8.decodeStream(request);
-              request.response.write("${body.split("").reversed.join()}\n");
-              break;
-          }
-          request.response.close();
+          logRequest(request);
+          final socket = await WebSocketTransformer.upgrade(request);
+          connections.add(socket);
+
+          socket.listen((final data) {
+            final dataDecoded = data.toString();
+            print("[+]Recieved data: $dataDecoded.");
+
+            for (final connection in connections) {
+              if (connection.readyState == WebSocket.open) {
+                connection.add(data);
+              }
+            }
+          }, onDone: () {
+            print("[-]${request.connectionInfo?.remoteAddress} disconnected.");
+						connections.remove(socket);
+          }, onError: logError, cancelOnError: true);
         });
       } catch (e) {
-        stderr.write("[!]Error: ${e.toString()}\n");
+        logError(e);
       }
     } catch (e) {
-      stderr.write("[!]Error: couldn't start server on $ip:$port\n");
+      stderr.write("[!]Error: couldn't start server on $ip:$port.\n");
     }
   }
 }
